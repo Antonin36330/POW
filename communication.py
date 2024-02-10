91b7f9e0d490a5d7d863f154c3dcb783be2d5960
Hugo Danet <61951279+cartesis07@users.noreply.github.com>
2024-02-10 14:51:18 -0800

    remove python files

--- communication.py
+++
@@ -1,7 +0,0 @@
-import slack_sdk
-import os
-
-def generate_msg(msg, channel):
-    slack_client = slack_sdk.WebClient(token=os.getenv("SLACK_BOT"))
-
-    slack_client.chat_postMessage(channel=channel, text=f"{msg}", mrkdwn=True)
